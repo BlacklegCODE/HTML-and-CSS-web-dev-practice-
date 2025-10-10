f106556aa706cffc57840d1f771dae38df39e338
Harsh Raundal <dkingston0000@gmail.com>
2025-10-10 22:28:30 +0530

    Create full HTML revisoin.php

---
+++ full HTML revisoin.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Final push</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <div>TODO write content</div>
+        <ul>
+            <li draggable="4"> this is first list
+                <ol>
+                    This
+                    or that
+                </ol>
+                <ol>
+                    second
+                    list
+                </ol>
+            </li>
+            <li>
+                this is second list
+                <ol>
+                    second this
+                    or that
+                </ol>
+                <ol>
+                    second 
+                    second list
+                </ol>
+            </li>
+        </ul>
+        
+        <a href="https://www.google.com/?zx=1760106339436&no_sw_cr=1">check this out</a>
+        
+        <h2>table practice</h2>
+        <table draggable="5" border ="2" cellpadding = "5">
+            <thead  colspan = "2" cellpadding = "3">
+            <tr border = "2" cellpadding = "3" >
+                tablets compare table   
+            </tr>
+            </thead>
+            <tbody>
+                <tr border = "2">
+                    <td colspan="2">Tablets</td>
+                    <td>Prices</td>
+                </tr>
+                <tr border = "3">
+                    <td colspan="2">
+                        Xiaomi pad 7 with
+                    </td>
+                    <td colspan="2">
+                        30,000
+                    </td>
+                </tr>
+                <tr border = "3">
+                    <td colspan="2">
+                        Lenovo Idea tab PRO
+                    </td>
+                    <td colspan="2">
+                        24,000
+                    </td>
+                </tr>
+                <tr border = "3">
+                    <td colspan="2">
+                        Lenovo Idea tab
+                    </td>
+                    <td colspan="2">
+                        16,000
+                    </td>
+                </tr>
+                
+            </tbody>
+        </table>
+        <br><!-- comment -->
+        <nav>Home | office</nav>
+        
+        <form>
+            <label for="name">Name</label>
+            <input type ="text" id = "name">
+            <br><!-- comment -->
+            <br><!-- comment -->
+            <label for="email">Email</label>
+            <input type="text" id ="mail">
+            <br> <br> 
+            
+            <label>Gender</label>
+            <input type ="radio">Male<!-- comment -->
+            <input type ="radio">Female
+            <br><br>
+            <label>Hobbies</label>
+            <input type ="checkbox">Singing
+            <input type ="checkbox">writing
+            <input type ="checkbox">dancing
+            <br><br>
+            <label>AGE</label>
+            <input type = "number" required="18">
+            <br><br>
+            <label>Give your feedback</label>
+            <br>
+            <textarea cols="50" required="5"></textarea>
+            <br><br>
+            <label>City</label>
+            <select>
+                <option>Malegaon</option>
+                <option>Nashik</option>
+                <option>Pune</option>
+                <option>Jalgaon</option>
+            </select>
+            <br><br>
+            <input type="reset" value="reset" />
+            <input type="submit" value="submit" />
+            <br><br>
+            <br><br>
+            <nav>Home | Office | School</nav>
+            <section><article><p>This is a demo website for my practice</p></article></section>
+            <p id = "info" style = "color: blue">This has multiple attributes</p>
+            <footer>&copy; This is my website practice !</footer>
+        </form>
+    </body>
+</html>
+
+
+
+
